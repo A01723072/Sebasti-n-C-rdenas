b757fcda3726965769496da528dee24fd9c116b2
A01723072 <cardenas2004.sebastian@gmail.com>
2025-04-07 20:41:48 -0600

    Update equipo1_calculadora_plan_de_retiro_py.py

--- equipo1_calculadora_plan_de_retiro_py.py
+++ equipo1_calculadora_plan_de_retiro_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1bJOFNr4EaliSffjgMMZWlKjTjCUX7scS
 """
 
-!pip install streamlit
+streamlit
 import streamlit as st
 import pandas as pd
 
